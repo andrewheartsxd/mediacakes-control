8f89dc499ca8b410d26338ab507af75ab3ed4fac
andrewheartsxd <andrewheartsxd@users.noreply.github.com>
2018-01-24 20:25:48 -0800

    more updates

--- firstRun.js
+++ firstRun.js
@@ -4,22 +4,24 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let smartcast = require('vizio-smart-cast');
+function writeConfig(smartcast) {
+  smartcast.discover((device) => {
+    console.log('Found: ', device);
+    let ip = device.ip;
+    let tv = new smartcast(ip);
 
-smartcast.discover((device) => {
-  console.log('Found: ', device);
-  //let ip = device.ip;
-  let ip = '192.168.29.149';
-  let tv = new smartcast(ip);
-
-  tv.pairing.initiate().then((response) => {
-    rl.question('Enter PIN:', (answer) => {
-      tv.pairing.pair(answer).then((response) => {
-        let authToken = response.ITEM.AUTH_TOKEN;
-        console.log('Auth token: ' + authToken);
-        const writeConfig = require('./writeConfig')({ ip: ip, authToken: authToken });
+    tv.pairing.initiate().then((response) => {
+      rl.question('Enter PIN:', (answer) => {
+        tv.pairing.pair(answer).then((response) => {
+          let authToken = response.ITEM.AUTH_TOKEN;
+          console.log('Auth token: ' + authToken);
+          const writeConfig = require('./writeConfig')({ ip: ip, authToken: authToken });
+        });
+        rl.close();
       });
-      rl.close();
     });
   });
-});
+}
+
+module.exports = writeConfig;
+
--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const wol = require('wol');
 
-let readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const smartcast = require('vizio-smart-cast');
 
-let smartcast = require('vizio-smart-cast');
-//smartcast.discover((device) => {
-  //console.log('Found: ', device);
-
-
-  //wol.wake('', function (err, res) {
-    //if (err) {
-      //console.log('err:', err);
-    //} else {
-      //tv.control.power.toggle();
-    //}
-  //});
-
-
-  //tv.pairing.initiate().then((respnse) => {
-    //rl.question('Enter PIN:', (answer) => {
-      //tv.pairing.pair(answer).then((response) => {
-        //console.log(response.ITEM.AUTH_TOKEN);
-      //});
-
-      //rl.close();
-    //});
-  //});
-
-//});
+try {
+  const config = require('./config.json');
+} catch (ex) {
+  const firstRun = require('./firstRun.js')(smartcast);
+}
