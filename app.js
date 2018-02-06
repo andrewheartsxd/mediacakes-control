b37bd85611f2ce48aabcce70646c7abe477ebce5
test <test@test.com>
2018-02-05 17:17:00 -0800

    Add wol component

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ try {
   // if config file exists, use config file to toggle power
   const config = require('./config.json');
   let tv = new smartcast(config.ip, config.authToken);
-  togglePower(tv);
+  // send wake on lan
+  wol.wake(config.macAddress, function (err, res) {
+    if (err) {
+      console.log('error sending wol:', err);
+    } else {
+      togglePower(tv);
+    }
+  });
 } catch (ex) {
   // otherwise, run firstRun.js and create config file, then toggle power
   console.log('First run, creating config file...');
--- firstRun.js
+++ firstRun.js
@@ -1,4 +1,5 @@
-let readline = require('readline');
+const arp = require('node-arp');
+const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -17,13 +18,23 @@ function firstRun(smartcast) {
         tv.pairing.pair(answer).then((response) => {
           let authToken = response.ITEM.AUTH_TOKEN;
           console.log('Auth token: ' + authToken);
+          let macAddress = arp.getMAC(ip, function (err, mac) {
+            if (err) {
+              console.log('error getting mac:', err);
+            } else {
+              console.log('mac address:', mac);
+              return mac;
+            }
+          });
           // write config file
-          const writeConfig = require('./writeConfig')({ ip: ip, authToken: authToken });
+          const writeConfig = require('./writeConfig')({ ip: ip, authToken: authToken, macAddress: macAddress});
         });
         rl.close();
         console.log('Run app.js again to toggle power');
       });
     });
+  }, (err) => {
+    console.log('error discovering device:', err);
   });
 }
 
